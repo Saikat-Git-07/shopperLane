b9fee49c5d3b19052539a7a8f64e3d0622b51158
Saikat-Git-07 <saikat1604@gmail.com>
2025-12-29 13:39:42 +0530

    Prepare backend and admin for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-const port = 4000;
+require("dotenv").config();
+const port = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
-const path = require("path");
 const cors = require("cors");
 const { type } = require("os");
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // database connection with mongodb
-mongoose.connect("Your MongoDB Connection String")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
@@ -74,7 +74,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
    res.json({
     success:1,
-    image_url:`http://localhost:${port}/images/${req.file.filename}`
+   image_url: `${process.env.BASE_URL}/images/${req.file.filename}`
    })
 })
 
@@ -175,7 +175,7 @@ app.post('/signup',async(req,res)=>{
         }
     }
 
-    const token = jwt.sign(data,'secret_ecom');
+    const token = jwt.sign(data, process.env.JWT_SECRET);
     res.json({success:true,token})
 
 })
@@ -191,7 +191,7 @@ app.post('/login', async(req,res)=>{
                     id:user.id
                 }
             }
-            const token = jwt.sign(data,'secret_ecom');
+            const token = jwt.sign(data, process.env.JWT_SECRET);;
             res.json({success:true,token});
         }
         else{
@@ -228,7 +228,7 @@ const fetchUser = async(req,res,next)=>{
     }
     else{
         try{
-            const data =jwt.verify(token,'secret_ecom');
+            const data =jwt.verify(token, process.env.JWT_SECRET);
             req.user = data.user;
             next();
         }
@@ -263,6 +263,7 @@ app.post('/getcart',fetchUser,async(req,res)=>{
     let userData = await Users.findOne({_id:req.user.id});
     res.json(userData.cartData);
 })
+
 
 app.listen(port,(error)=>{
     if(!error){
